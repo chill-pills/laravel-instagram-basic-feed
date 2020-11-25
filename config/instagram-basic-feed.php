4ddfd1aa06249c603306bcc9d75bf8648c70a0af
OtienoJulie <otienojulie@gmail.com>
2020-11-25 06:17:33 +0300

    wip: adds command to get loginUrl()

--- config/instagram-basic-feed.php
+++ config/instagram-basic-feed.php
@@ -3,6 +3,15 @@
 return [
 
     /*
+     * These are the information used to configure the Instagram Basic Display application.
+     * Read the guide here (https://developers.facebook.com/docs/instagram-basic-display-api/getting-started).
+     */
+    'instagram_valid_oauth_uri' => env('INSTAGRAM_VALID_OAUTH_URI'),
+    'instagram_app_id' => env('INSTAGRAM_APP_ID'),
+    'instagram_secret_key' => env('INSTAGRAM_SECRET_KEY'),
+    'instagram_app_scope' => env('INSTAGRAM_APP_SCOPE','user_profile,user_media'),
+
+    /*
      * Generate an access token for a specific user.
      * This token let you fetch only the user's feed
      */
---
+++ src/Console/InstagramGenerateAuthorizationUrl.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ChillPills\InstagramBasicFeed\Console;
+
+use EspressoDev\InstagramBasicDisplay\InstagramBasicDisplay;
+use Illuminate\Console\Command;
+
+class InstagramGenerateAuthorizationUrl extends Command
+{
+    protected $signature = 'instagram-feed:get-authorization-url';
+
+    protected $description = 'Generates the link to the Authorization Window page';
+
+    public function handle()
+    {
+        $this->instagramBasicDisplay = new InstagramBasicDisplay([
+            'appId' => config('instagram-feed.instagram_app_id'),
+            'appSecret' => config('instagram-feed.instagram_secret_key'),
+            'redirectUri' => config('instagram-feed.instagram_valid_oauth_uri'),
+        ]);
+
+        $this->info($this->instagramBasicDisplay->getLoginUrl());
+    }
+}
